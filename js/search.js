829fd1d991ee88d5c9209bbb6889b9ceda39253e
twishasaraiya <saraiyatwisha@gmail.com>
2017-12-11 19:28:20 +0530

    search playlist

--- js/search.js
+++ js/search.js
@@ -3,6 +3,7 @@ function(ytData, playlist, template, $) {
     console.log(ytData, playlist, template, $);
     var searchTrigger = null;
     var searchRes = null;
+    var searchPlaylist= null;
 
     var triggerSearch = function(e) {
         var key = e.keyCode || e.which;
@@ -17,6 +18,17 @@ function(ytData, playlist, template, $) {
 
     var searchVid = function(params){
         if(params.length>0){
+            searchPlaylist={};
+            var pattern=new RegExp(params,'i');
+            for(var si=0;si<playlist.size();si++){
+                var song=playlist.get(si);
+                var title=song.snippet.title;
+                if(title.search(pattern)!==-1){
+                    //console.log(song);
+                    var id=song.id.videoId;
+                    searchPlaylist[id]=song;
+                }
+            }
             ytData.api.search.list({
                 q:params,
                 part:'snippet',
@@ -36,7 +48,30 @@ function(ytData, playlist, template, $) {
     }
 
     var displaySearchResults = function(){
-        $("#search_res").empty()
+        $("#search_res").empty();
+        $("#search_res").append("<li><h6 class='ti'>Playlist</h6></li>");
+        if(searchPlaylist===null){
+            $("#search_res").append("<p>Nothing found</p>");
+        }else{
+            for(song in searchPlaylist){
+                var vidrow=template.getVidRow(searchPlaylist[song]);
+                $(vidrow).addClass("item-search");
+                $(vidrow).on("click",function(){
+                    //console.log('clicked '+this.id);
+                    var hashId="#playlist > #"+this.id;
+                    $("#playlist").animate({
+                            scrollTop : $(hashId).offset().top
+                        }, 2000);
+                    $(hashId).css({'background-color':'#56BD81'});
+                    setTimeout(function(){
+                        $(hashId).css({'background-color':'#fff'});
+                    },3000);
+                });
+                $(vidrow).find("#action").css({'display': 'none'});
+                vidrow.appendTo("#search_res");
+            }
+        }
+        $("#search_res").append("<li><h6 class='ti'>Youtube</h6></li>");
         for(result in searchRes){
             var vidrow=template.getVidRow(searchRes[result]);
             $(vidrow).addClass("item-search");
