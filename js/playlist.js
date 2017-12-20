2e9ed283ad317886c97134983a086ba0530a586a
aniketgattani <aniketgattani@gmail.com>
2017-12-20 19:43:44 +0530

    Multiple Playlists Added

--- js/playlist.js
+++ js/playlist.js
@@ -1,9 +1,11 @@
 define(['ytDataAPI', 'ytIframeAPI', 'template','jquery'],
 function(ytData,ytIframe,template,$) {
     var playlist=[];
+    var durations=[];
+    var repeat = [];
     var total_duration = 0;
-    var playlistChangeListeners = []
-
+    var playlistChangeListeners = [];
+    var curr_id;
     var setup = function() {
         // Load playlist from cache/url
         if($.query.playlist!=undefined){
@@ -12,7 +14,7 @@ function(ytData,ytIframe,template,$) {
                 part:'snippet'
             }).execute(function(response){
                 response.items.forEach(function(r){
-                    playlist.push({'id':{'videoId':r.id},'snippet':r.snippet});
+                    playlist[curr_id].push({'id':{'videoId':r.id},'snippet':r.snippet});
                 });
 
                 console.log('fetched playlist');
@@ -21,10 +23,13 @@ function(ytData,ytIframe,template,$) {
         } else {
             if(JSON.parse(localStorage.getItem('new_playlist_saved'))) {
                 playlist = JSON.parse(localStorage.getItem('playlist'));
-                var rptst = localStorage.getItem('repeat');
-
-                total_duration = JSON.parse(localStorage.getItem('total_duration'));
+                if (curr_id==undefined) curr_id=playlist.length-1;
+                repeat = JSON.parse(localStorage.getItem('repeat'));
+                var rptst = repeat[curr_id];
+                durations=JSON.parse(localStorage.getItem('duration'));
+                total_duration = durations[curr_id];
                 updateDurationOnDisplay();
+                addPlaylistDropdown();
 
                 if(rptst=='one')
                     repeat_one=true;
@@ -32,25 +37,27 @@ function(ytData,ytIframe,template,$) {
                     repeat_all=true;
                 else if(rptst=='shuffle')
                     shuffle=true;
-
                 console.log("loaded playlist");
             }
         }
-
-        // Display all playlist songs
-        for(var i=0; i<playlist.length; i++){
-            var vidrow=template.getVidRow(playlist[i]);
-            var id=playlist[i].id.videoId;
-            $(vidrow).addClass("item-video waves-effect");
-            $(vidrow).on("click", function(id){return function(event){
-                require('player').playSong(id);
-            }}(id));
-            $(vidrow).find("#action").on("click",function(id){return function(event){
-                removeFromPlaylist(event,id);
-            }}(id));
-            $(vidrow).find("#action i").html("clear");
-            vidrow.appendTo("#playlist");
+        if (playlist.length!=0){
+            // Display all playlist songs
+            for(var i=0; i<playlist[curr_id].length; i++){
+                var vidrow=template.getVidRow(playlist[curr_id][i]);
+                var id=playlist[curr_id][i].id.videoId;
+                $(vidrow).addClass("item-video waves-effect");
+                $(vidrow).on("click", function(id){return function(event){
+                    require('player').playSong(id);
+                }}(id));
+                $(vidrow).find("#action").on("click",function(id){return function(event){
+                    removeFromPlaylist(event,id);
+                }}(id));
+                $(vidrow).find("#action i").html("clear");
+                vidrow.appendTo("#playlist");
+            }
         }
+        else addPlaylist();
+        $('#playlist_header').text("Playlist "+(curr_id+1));
     }
 
     var parseDuration = function(duration, sign){
@@ -94,7 +101,8 @@ function(ytData,ytIframe,template,$) {
     }
 
     var saveDuration = function() {
-        localStorage.setItem('total_duration', JSON.stringify(total_duration));
+        durations[curr_id]=total_duration;
+        localStorage.setItem('duration', JSON.stringify(durations));
     }
 
     var savePlaylist = function() {
@@ -102,22 +110,27 @@ function(ytData,ytIframe,template,$) {
         localStorage.setItem('new_playlist_saved', JSON.stringify(true));
     }
 
+    var saveRepeat = function(mode){
+        repeat[curr_id] = mode;
+        localStorage.setItem('repeat', JSON.stringify(repeat));
+    }
+
     var addToPlaylist = function(viddata){
         // fetch length of video and add to playtime
         getContentDuration(viddata.id.videoId);
 
         var j=0;
-        for(var i=0; i<playlist.length; i++){
-            if(viddata.id.videoId==playlist[i].id.videoId)
+        for(var i=0; i<playlist[curr_id].length; i++){
+            if(viddata.id.videoId==playlist[curr_id][i].id.videoId)
                 break;
             j++;
         }
-        if(j<playlist.length){
+        if(j<playlist[curr_id].length){
             Materialize.toast('Song already exists in playlist!', 2000);
             return;
         }
 
-        playlist.push(viddata);
+        playlist[curr_id].push(viddata);
         savePlaylist();
         //displayPlaylist();
 
@@ -141,15 +154,15 @@ function(ytData,ytIframe,template,$) {
 
     var removeFromPlaylist = function(event,id){
         var index=0;
-        for(var i=0; i<playlist.length; i++){
-            if(id==playlist[i].id.videoId)
+        for(var i=0; i<playlist[curr_id].length; i++){
+            if(id==playlist[curr_id][i].id.videoId)
                 break;
             index++;
         }
         console.log("remove: "+index, id);
         getContentDuration(id, -1);
 
-        playlist.splice(index,1);
+        playlist[curr_id].splice(index,1);
         savePlaylist();
 
         $("#playlist #"+id).animate({'opacity':'0'},{duration:300,easing:'linear'});
@@ -171,7 +184,7 @@ function(ytData,ytIframe,template,$) {
 
     var getPlaylistIds = function(){
         var ids = [];
-        playlist.forEach(function(vid){
+        playlist[curr_id].forEach(function(vid){
             ids.push(vid.id.videoId);
         });
         return ids;
@@ -182,14 +195,14 @@ function(ytData,ytIframe,template,$) {
     }
 
     var moveSong = function(from, to, song){
-        playlist.splice(from, 1);
-        playlist.splice(to, 0, song);
+        playlist[curr_id].splice(from, 1);
+        playlist[curr_id].splice(to, 0, song);
         savePlaylist();
     }
 
     var clearPlaylist = function(){ 
         ytIframe.player.stopVideo();
-        playlist = [];
+        playlist[curr_id]=[];
         total_duration = 0;
         $('#playlist li').remove();
         $('#totalDuration').text("00:00:00");
@@ -198,7 +211,43 @@ function(ytData,ytIframe,template,$) {
         document.title= "Youtube Fast Playlist";
     }
 
+    var addPlaylist = function(){
+        if(ytIframe.player!=undefined) ytIframe.player.stopVideo();
+        curr_id=playlist.length;
+        total_duration = 0;
+        $('#totalDuration').text("00:00:00");
+        $('#playlist li').remove();
+        playlist[curr_id]=[];
+        savePlaylist();
+        saveDuration();
+        saveRepeat();
+        localStorage.setItem('curr_id',curr_id);
+        document.title= "Youtube Fast Playlist";
+        setup();
+    }
+
+    var addPlaylistDropdown = function(){
+        var lis=$('#playlists li').length;
+        for (var i = lis ; i < playlist.length; i++) {
+            $('#playlists').append('<li id='+i+'><br>Playlist'+(i+1)+'</br></li>');
+            $($('#playlists li')[i]).on('click', function(){
+                changePlaylist(event);
+            });
+        }
+
+    }
+
+    var changePlaylist= function(event){
+        if(ytIframe.player!=undefined) ytIframe.player.stopVideo();
+        curr_id=parseInt(event.target.id);
+        localStorage.setItem('curr_id',curr_id);
+        $('#playlist li').remove();
+        localStorage.setItem('playing',-1);
+        setup();
+    }
+
     setup();
+    
 
     return {
         savePlaylist: savePlaylist,
@@ -209,11 +258,13 @@ function(ytData,ytIframe,template,$) {
         addListener: addListener,
         moveSong: moveSong,
         clearPlaylist: clearPlaylist,
-        size: function() {
-            return playlist.length;
+        addPlaylist: addPlaylist,
+        saveRepeat: saveRepeat,
+        size: function(){
+            return playlist[curr_id].length;
         },
         get: function(index) {
-            return playlist[index];
-        }
+            return playlist[curr_id][index];
+        },
     }
 });
